6e8f1150e852cda0500b2e9f07f2fb2cb8fc404b
Miguel Pombo <miguel.pomboshape@modec.com>
2024-06-14 13:06:15 -0300

    fix: adicionando like e dislike nos topicos

--- src/components/TopicCard/index.tsx
+++ src/components/TopicCard/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { HiOutlineHeart } from "react-icons/hi";
-import { FaRegComment } from "react-icons/fa";
+import { FaRegComment, FaRegThumbsDown, FaRegThumbsUp } from "react-icons/fa";
 
 import AvatarImage from "../AvatarImage";
 import { CardContainer, CommentsAndLikesContainer, TopicDescription, TopicInformationsContainer, TopicTitle } from "./style";
@@ -10,7 +9,20 @@ import { ITopicCardsComponentProps } from '../../interfaces/Topics';
 function TopicCard (topicProps: ITopicCardsComponentProps) {
   const navigate = useNavigate()
   const [topicLiked, setTopicLiked] = useState(false)
+  const [topicDisliked, setTopicDisliked] = useState(false)
   const [windowWidth] = useState(document.documentElement.clientWidth)
+
+  const handleLikesAndDislike = (optionRequired: string) => {
+    if (optionRequired === 'LIKE') {
+      setTopicLiked(true)
+      setTopicDisliked(false)
+    }
+
+    if (optionRequired === 'DISLIKE') {
+      setTopicDisliked(true)
+      setTopicLiked(false)
+    }
+  }
 
   return (
     <CardContainer>
@@ -24,11 +36,11 @@ function TopicCard (topicProps: ITopicCardsComponentProps) {
         <TopicDescription>{topicProps.topicDescription}</TopicDescription>
       </TopicInformationsContainer>
       <CommentsAndLikesContainer>
-        <span><HiOutlineHeart size={windowWidth > 979 ? 30 : 25} onClick={() => setTopicLiked(!topicLiked)} fill={topicLiked ? '#ff2929' : '#ffff'}/>{topicProps.likesQuantity}</span>
+        <span><FaRegThumbsUp size={windowWidth > 979 ? 30 : 25} onClick={() => handleLikesAndDislike("LIKE")} fill={topicLiked ? '#19dd19' : '#000'}/>{topicProps.likesQuantity}</span>
+        <span><FaRegThumbsDown size={windowWidth > 979 ? 30 : 25} onClick={() => handleLikesAndDislike("DISLIKE")} fill={topicDisliked ? '#ff2929' : '#000'}/>{topicProps.likesQuantity}</span>
         <span><FaRegComment size={windowWidth > 979 ? 30 : 25} fill=''/>{topicProps.commentsQuantity}</span>
       </CommentsAndLikesContainer>
     </CardContainer>
   )
 }
-
 export default TopicCard;
