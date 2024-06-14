4ab62c77c1d75bf113bf5067cf2217043fe3dc7e
Miguel Pombo <miguel.pomboshape@modec.com>
2024-06-14 09:48:28 -0300

    fix: adicionando array de dependencia em useEffect ja feitos

--- src/pages/Groups/index.tsx
+++ src/pages/Groups/index.tsx
@@ -20,9 +20,7 @@ function Groups () {
     }
 
     fetchData()
-  })
-
-
+  }, [])
 
   return (
     <Container>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -46,7 +46,7 @@ function Home () {
     }
 
     fetchData()
-  })
+  }, [])
 
   return (
     <Container>
